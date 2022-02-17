7f019135e7b6c15e73f570e54d866bfeed6ecb15
Yogapratama1109 <yoga110011@gmail.com>
2022-02-17 20:19:28 +0700

    init

---
+++ dist/assets/js/dashboard.js
@@ -0,0 +1,16 @@
+$(document).ready( function () {
+    $('#tableBri').DataTable({
+        scrollX : true,
+        "columns": [
+            { "width": "5%" },
+            { "width": "10%" },
+            { "width": "15%" },
+            { "width": "15%" },
+            { "width": "15%" },
+            { "width": "10%" },
+            { "width": "10%" },
+            { "width": "10%" },
+            { "width": "10%" },
+        ],
+    });
+} );
